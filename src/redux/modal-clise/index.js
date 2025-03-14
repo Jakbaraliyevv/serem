db1e5e601787a06f33d9e73c0155b114e62d539b
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-03-14 21:27:26 +0500

    create

--- src/redux/modal-clise/index.js
+++ src/redux/modal-clise/index.js
@@ -1,7 +1,28 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   isOpen: false, // Modal dastlab yopiq
+// };
+
+// const modalSlice = createSlice({
+//   name: "modal",
+//   initialState,
+//   reducers: {
+//     toggleModal: (state) => {
+//       state.isOpen = !state.isOpen;
+//     },
+//   },
+// });
+
+// export const { toggleModal } = modalSlice.actions;
+// export default modalSlice.reducer;
+// 1. modal-slice.js faylini o'zgartiramiz
+// 1. modal-slice.js faylini o'zgartiramiz
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isOpen: false, // Modal dastlab yopiq
+  isOpen: false,
+  refreshData: false, // Yangi state qo'shamiz
 };
 
 const modalSlice = createSlice({
@@ -11,9 +32,11 @@ const modalSlice = createSlice({
     toggleModal: (state) => {
       state.isOpen = !state.isOpen;
     },
+    triggerRefresh: (state) => {
+      state.refreshData = !state.refreshData; // Refresh triggerini o'zgartiramiz
+    },
   },
 });
 
-export const { toggleModal } = modalSlice.actions;
-
+export const { toggleModal, triggerRefresh } = modalSlice.actions;
 export default modalSlice.reducer;
